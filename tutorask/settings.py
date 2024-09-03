43750f54bb6f80308e599dfcf38752d16f1bb914
Andrea Siena <58293988+CodKyrat47@users.noreply.github.com>
2024-09-03 17:16:37 +0200

    Fixing email credential on settings.py

--- tutorask/settings.py
+++ tutorask/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noreply.tutorask2024@gmail.com'
-EMAIL_HOST_PASSWORD = 'ntgl zudw pvtz nsgg'
+EMAIL_HOST_USER = ''  # insert own email host user here
+EMAIL_HOST_PASSWORD = ''  # insert own email host password here
